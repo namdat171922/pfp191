9bbd9776baf5e7f359c225fe41ec66fecf4b9fd0
namdat171922 <namdat171922@gmail.com>
2024-07-10 13:14:07 +0700

    Add files via upload

---
+++ count_vowel = 0.py
@@ -0,0 +1,13 @@
+start = 0 
+def create_list(start,end):
+    x = create_list
+    while True: 
+        if start < 10: 
+            start = start + 1
+            print (start - 1, start) 
+            continue
+        if start == 10: 
+            print (end)
+            break
+    print(create_list(x))
+
